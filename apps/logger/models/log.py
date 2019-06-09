b45bb32602c3962dce1ef06cfd5785946fb9f529
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-06-09 23:27:28 +0300

    Remove apps from installed apps

---
+++ apps/logger/models/log.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Log(models.Model):
+    """Log Model"""
+
+    ip_address = models.GenericIPAddressField(verbose_name='IP адрес', protocol='both')
+    created_at = models.DateTimeField(verbose_name='Дата и время')
+    method = models.CharField(verbose_name='HTTP метод', max_length=10)
+    uri = models.TextField(verbose_name='URI')
+    code = models.PositiveIntegerField(verbose_name='Код ответа')
+    size = models.PositiveIntegerField(verbose_name='Размер в байтах')
